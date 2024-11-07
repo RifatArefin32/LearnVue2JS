ea1a46dd79104671799e0ae60a52bd284fa45cde
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-11-07 14:03:41 +0600

    2. Vue data and methods

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ new Vue({
     el: '#vue-app', 
     data: {
         name: 'Rifat Arefin',
+        job: 'Software Engineering',
     },
+    methods: {
+        greet: function(time) {
+            return 'Good ' + time + ' ' + this.name ;
+        }
+    }
 
 });
