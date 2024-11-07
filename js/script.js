fe843323378349fdad43bbc81e9ef2c275e909e5
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-11-07 14:15:54 +0600

    3. Vue data binding to differnet html attributes

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ new Vue({
     data: {
         name: 'Rifat Arefin',
         job: 'Software Engineering',
+        website: 'https://chatgpt.com/',
+        websiteTemplate: '<a href="https://chatgpt.com/">My website</a>',
     },
     methods: {
         greet: function(time) {
